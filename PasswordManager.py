e5bc73c54e43a76e531f9526d2b249050c8906d4
mbeier08 <madelynmbeier@gmail.com>
2025-05-08 09:48:38 -0500

    created classes

--- PasswordManager.py
+++ PasswordManager.py
@@ -1,163 +1,10 @@
+from Tools import *
 import random
 from random import shuffle
 import pandas as pd
 credentials = False
 tries = 0
 accounts = pd.read_csv("accounts.csv")
-
-
-def passwordGenerator():
-    def shuffle_password(word):  
-        word = list(word)
-        shuffle(word)
-        return ''.join(word)
-
-    numUpper = int(input("How many capital letters do you want in your password?  "))
-    numLower = int(input("Lowercase letters?  "))
-    numNumbers = int(input("Numbers?  "))
-    numSpec = int(input("Special characters?  "))
-
-    special = ["!","@","#","$","%","^","&","*","(",")"]
-    password = ""
-
-    u=1
-    for u in range (1,numUpper+1):
-        rand = random.randint(65,90)
-        letter = chr(rand)                      
-        password = password + letter
-        letter = ""
-    l=1
-    for l in range (1,numLower+1):
-        rand = random.randint(97,122)
-        letter = chr(rand)
-        password = password + letter
-        letter = ""
-    n=1
-    for n in range (1,numNumbers+1):
-        rand = str(random.randint(0,9))
-        password = password + rand
-    s=1
-    for s in range (1,numNumbers+1):
-        rand = random.randint(0,9)
-        char = special[rand]
-        password = password + char
-
-    print(shuffle_password(password))
-    return password
-
-
-def intialUserLogin(credentials):
-    existingUser = input("Do you have an account? (y/n)")
-    
-    if existingUser == "y":
-        username = accounts["username"][userUser]
-        password = accounts["password"][userPass]
-        hint = accounts["hint"][userUser]
-        print(hint)
-    else:
-        print("Create a Profile:")
-        category = input("What type of account is this? ")
-        username = input("Username: ")
-        password = input("Password: ")   # Make sure the password meets requirements
-        hint = input("Password Hint: ")
-        first = input("First Name: ")
-        last = input("Last Name: ")
-        accounts.loc[len(accounts[])]
-    tries=0
-    userUser=""
-    userPass=""
-    while credentials != True:
-        if tries <3:
-            if userUser==username and userPass==password:  #however you check if the credentials are correct
-                credentials = True
-            else:
-                userUser = input("Username:  ")
-                print(f"Password Hint: ______")  # Access the hint from the text file and print to screen
-                userPass = input("Password:  ")
-                if userUser!=username and userPass!=password:
-                    tries+=1
-                if tries>0:
-                    print("Incorrect username or password, try again")
-        else:
-            print("Sorry, you have too many invalid login attempts. Close the program and try again.")
-            exit()
-
-
-def newAccount():
-    category = input("Category (Home, Work, Entertainment, Bills): ")
-    name = input("Account Name: ")
-    username = input("Username: ")
-    password = input("Password: ")   # Make sure the password meets requirements
-    hint = input("Password Hint: ")
-
-
-def viewByCategory():
-    #print out the categories that they can filter by
-    #obtain which category to filter by
-    whichCat = input("Which category do you want to filter by?  ")
-
-    #read the data and save to a list
-    
-
-
-def viewAccount():
-    whichAcc = input("Which account would you like to change?  ")
-    
-    with open("accounts.csv","r") as file:
-        lines = file.readlines() #converts file to list
-    for i in range(len(lines)):
-        category,name,username,password,hint = lines[i].strip().split(",")
-        if whichAcc == name:
-           print(f'''
-                 The account: {name}
-                 The username: {username}
-                 The password: {password}
-                 ''')
-
-
-def changeAccount():
-    whichAcc = input("Which account would you like to delete?  ")
-    
-    #viewAccount()  Run the viewAccount function to show them the current stored information
-
-    #get data that they want to change
-
-    #read in all the data aka save the file to a list
-    with open("accounts.csv","r") as file:
-        lines = file.readlines() #converts file to list
-    #find the old account
-    for i in range(len(lines)):
-        category,name,username,password,hint = lines[i].strip().split(",")  # Will this work with more than 2 variables?
-        if name == old:
-            name = new
-            newData = f"{category},{name},{username},{password},{hint}\n"
-            lines[i] = newData
-    print(lines) 
-    #overwrite the file
-
-
-
-def deleteAccount():
-    whichAcc = input("Which account would you like to delete?  ")
-    newLines = []
-    found = False
-    with open("accounts.csv","r") as file:
-        lines = file.readlines() #converts file to list
-    for i in range(len(lines)):
-        category,name,username,password,hint = lines[i].strip().split(",")
-        if whichAcc != name:
-            newLines.append(i)
-        else: 
-            found = True
-            
-    #Tell them if the account is not found
-    if not found:
-        print("The account was not found.")
-        return
-    
-    with open("accounts.csv","r") as file:
-        file.writelines(newLines)
-           
 
 
 def invalid_choice():
@@ -167,23 +14,31 @@ def exit():
     print("Goodbye!")
     raise SystemExit  #stops the program
 
-
+def newAccount(accounts):
+        category = input("Category (Home, Work, Entertainment, Bills): ")
+        name = input("Account Name: ")
+        username = input("Username: ")
+        password = input("Password: ")   # Make sure the password meets requirements
+        hint = input("Password Hint: ")
+        
+        listOfAccounts.append(Account(category,name,username,password,hint))
+        ## Need something after this to actually write to the file    
 
 
 # MAIN LOOP
 
 options = {
         "1":newAccount,
-        "2":viewAccount,
-        "3":viewByCategory,
-        "4":passwordGenerator,
-        "5":changeAccount,
-        "6":deleteAccount,
+        "2":Account.viewAccount,
+        "3":Account.viewByCategory,
+        "4":Passwords.passwordGenerator,
+        "5":Account.changeAccount,
+        "6":Account.deleteAccount,
         "7":exit
         }
 
 #Login
-intialUserLogin(False)
+# intialUserLogin(False)
 
 while True:
     print("\n--- Password Manager ---")
---
+++ Tools.py
@@ -0,0 +1,144 @@
+from random import shuffle
+import random
+special = ["!","@","#","$","%","^","&","*","(",")"]
+
+class Passwords:
+        
+    def passwordGenerator():
+        numUpper = int(input("How many capital letters do you want in your password?  "))
+        numLower = int(input("Lowercase letters?  "))
+        numNumbers = int(input("Numbers?  "))
+        numSpec = int(input("Special characters?  "))
+
+        special = ["!","@","#","$","%","^","&","*","(",")"]
+        password = ""
+
+        u=1
+        for u in range (1,numUpper+1):
+            rand = random.randint(65,90)
+            letter = chr(rand)
+            password = password + letter
+            letter = ""
+        l=1
+        for l in range (1,numLower+1):
+            rand = random.randint(97,122)
+            letter = chr(rand)
+            password = password + letter
+            letter = ""
+        n=1
+        for n in range (1,numNumbers+1):
+            rand = str(random.randint(0,9))
+            password = password + rand
+        s=1
+        for s in range (1,numNumbers+1):
+            rand = random.randint(0,9)
+            char = special[rand]
+            password = password + char
+
+        word = list(password)
+        shuffle(word)
+        return ''.join(word)
+    
+    def passwordValidator(password):
+        hasUpper=False
+        hasLower=False
+        hasDigit=False
+        hasSpecial=False
+        
+        if len(password) < 8:
+            return False, "Password must be at least 8 characters long"
+        
+        #Check for the characters
+        for char in password:
+            if char.isupper():
+                hasUpper = True
+            elif char.islower():
+                hasLower = True
+            elif char.isdigit():
+                hasDigit = True
+            elif char in special:
+                hasSpecial = True
+        
+        #Return a corresponding message if one test fails
+        if not hasUpper:
+            return False, "Password needs at least one uppercase letter"
+        if not hasLower:
+            return False, "Password needs at least one lowercase letter"
+        if not hasDigit:
+            return False, "Password needs at least one digit"
+        if not hasSpecial:
+            return False, "Password needs at least one special character"
+        
+        return True, "Password passes all tests!"
+    
+
+class Account:
+    def __init__(self, filename="accounts.csv"):
+        self.filename = filename
+    
+    
+    def viewAccount(accounts):
+        whichAcc = input("Which account would you like to change?  ")
+        
+        for i in range(len(accounts,name,username,password)):
+            category,name,username,password,hint = accounts[i].strip().split(",")
+            if whichAcc == name:
+                print(f'''
+                        The account: {name}
+                        The username: {username}
+                        The password: {password}
+                        ''')
+                
+                
+    def viewByCategory(accounts):
+        #print out the categories that they can filter by
+        #obtain which category to filter by
+        whichCat = input("Which category do you want to filter by?  ")
+
+        filtered = accounts[accounts['whichCat'].str.low() == whichCat.lower()]
+        
+        print(f'Accounts under category {whichCat}')
+        for name in filtered['name']:
+            print(name)
+            
+    
+    
+        
+        
+    def changeAccount(accounts):
+        whichAcc = input("Which account would you like to delete?  ")
+        
+        #viewAccount()  Run the viewAccount function to show them the current stored information
+
+        #get data that they want to change
+
+        #read in all the data aka save the file to a list
+       
+        #find the old account
+        for i in range(len(accounts)):
+            category,name,username,password,hint = accounts[i].strip().split(",")
+            # Need to add something else to make this actually work
+
+
+    def deleteAccount(accounts):
+        whichAcc = input("Which account would you like to delete?  ")
+        newLines = []
+        found = False
+        with open("accounts.csv","r") as file:
+            lines = file.readlines() #converts file to list
+        for i in range(len(lines)):
+            category,name,username,password,hint = accounts[i].strip().split(",")
+            if whichAcc != name:
+                newLines.append(i)
+            else: 
+                found = True
+                
+        #Tell them if the account is not found
+        if not found:
+            print("The account was not found.")
+            return
+        
+        with open("accounts.csv","r") as file:
+            file.writelines(newLines)
+            
+                        
